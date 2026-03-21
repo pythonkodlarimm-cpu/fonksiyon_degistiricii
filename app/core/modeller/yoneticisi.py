5cd11f6f32e91d86cc76d3b67ad018d21080d400
FY <pythonkodlarimm@gmail.com>
2026-03-21 08:18:56 +0300

    Add files via upload

---
+++ app/core/modeller/yoneticisi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+DOSYA: app/core/modeller/yoneticisi.py
+"""
+
+from __future__ import annotations
+
+
+class ModellerYoneticisi:
+    def function_item_sinifi(self):
+        from app.core.modeller.modeller import FunctionItem
+        return FunctionItem
