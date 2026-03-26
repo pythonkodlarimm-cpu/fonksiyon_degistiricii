e2ffed32b3d67102840bd4782e1640b157749d64
FY <pythonkodlarimm@gmail.com>
2026-03-27 01:07:04 +0300

    Create __init__.py

---
+++ app/services/sistem/diller/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+DOSYA: app/services/sistem/diller/__init__.py
+
+ROL:
+- Dil JSON dosyalarının bulunduğu paket klasörünü işaretler
+- Paketleme sırasında klasörün modül olarak tanınmasını sağlar
+
+SURUM: 1
+TARIH: 2026-03-23
+IMZA: FY.
+"""
